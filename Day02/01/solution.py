bbc9c19893e4bc361d58943dedddcf14a241685a
Travis CI <travis@travis-ci.org>
2020-12-04 15:30:52 +0530

    Clean up of Day 2's solution

--- Day02/01/solution.py
+++ Day02/01/solution.py
@@ -1,20 +1,19 @@
 
+def getCharCountLimits(stringyfiedLimits):
+	[lowerLimit, upperLimit] = stringyfiedLimits.split('-')
+	return [int(lowerLimit), int(upperLimit)]
+
+def isPolicySatisfied(policyAndPassword):
+	[stringyfiedLimits, rawletter, password] = policyAndPassword.split(' ')
+	[lowerLimit, upperLimit] = getCharCountLimits(stringyfiedLimits)
+	[letter, empty] = rawletter.split(':')
+	letterCount = len(filter(lambda char: char == letter, password))
+	return (lowerLimit <= letterCount) and (letterCount <= upperLimit)
+
 def solution():
 	inputFile = open('../input.txt', 'r') 
-	passwordFile = inputFile.readlines()
-	validPasswordCount = 0
-	for line in passwordFile:
-		[charRange, rawletter, password] = line.split(' ')
-		[lowerRange, upperRange] = charRange.split('-')
-		lowerRange = int(lowerRange)
-		upperRange = int(upperRange)
-		[letter, empty] = rawletter.split(':')
-		letterCount = 0
-		for char in password:
-			if char == letter:
-				letterCount += 1
-		if (lowerRange <= letterCount) and (letterCount <= upperRange):
-			validPasswordCount += 1
-	print (validPasswordCount)
+	dbLines = inputFile.readlines()
+	validPasswords = filter(lambda dbLine: isPolicySatisfied(dbLine), dbLines)
+	print len(validPasswords)
 
 solution()
--- Day02/02/solution.py
+++ Day02/02/solution.py
@@ -1,21 +1,25 @@
+def xor(bool1, bool2):
+	return bool1 != bool2
+
+def isCharInOnlyOnePosition(letter, password, position1, position2):
+	isCharInFirstPosition = password[position1 - 1] == letter
+	isCharInSecondPosition = password[position2 - 1] == letter
+	return xor(isCharInFirstPosition, isCharInSecondPosition)
+
+def getCharCountPositions(stringyfiedPositions):
+	[position1, position2] = stringyfiedPositions.split('-')
+	return [int(position1), int(position2)]
+
+def isPolicySatisfied(policyAndPassword):
+	[stringyfiedPositions, rawletter, password] = policyAndPassword.split(' ')
+	[position1, position2] = getCharCountPositions(stringyfiedPositions)
+	[letter, empty] = rawletter.split(':')
+	return isCharInOnlyOnePosition(letter, password, position1, position2)
 
 def solution():
 	inputFile = open('../input.txt', 'r') 
-	passwordFile = inputFile.readlines()
-	validPasswordCount = 0
-	for line in passwordFile:
-		[charRange, rawletter, password] = line.split(' ')
-		[posOne, posTwo] = charRange.split('-')
-		posOne = int(posOne)
-		posTwo = int(posTwo)
-		[letter, empty] = rawletter.split(':')
-		letterCount = 0
-		if (password[posOne - 1] == letter):
-			if (password[posTwo - 1] != letter):
-				validPasswordCount += 1
-		else:			
-			if (password[posTwo - 1] == letter):
-				validPasswordCount += 1
-	print (validPasswordCount)
+	dbLines = inputFile.readlines()
+	validPasswords = filter(lambda dbLine: isPolicySatisfied(dbLine), dbLines)
+	print len(validPasswords)
 
 solution()
