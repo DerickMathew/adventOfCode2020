3ef19e7d054dadf9f1ddae856a3fe11db0ffd16c
Travis CI <travis@travis-ci.org>
2020-12-04 14:56:14 +0530

    Clean up of Day 1's solution

--- Day01/01/solution.py
+++ Day01/01/solution.py
@@ -1,17 +1,18 @@
-YEAR = 2020
+def findNumbersThatAddUpTo(numbers, year):
+	for i in xrange(len(numbers)):
+		difference = year - numbers[i]
+		if difference in  numbers[i+1:]:
+			return [numbers[i], difference]
 
-def getExpense(expenseReport, year):
-	for i in xrange(len(expenseReport)):
-		for j in xrange(i+1, len(expenseReport)):
-			if (expenseReport[i] + expenseReport[j]) == year:
-				return (expenseReport[i] * expenseReport[j])
+def getLinesFromInput():
+	inputFile = open('../input.txt', 'r') 
+	return inputFile.readlines()
 
 def solution():
-	inputFile = open('../input.txt', 'r') 
-	reportLines = inputFile.readlines()
-	expenseReport = []
-	for n in reportLines:
-		expenseReport.append(int(n))
-	print (getExpense(expenseReport, YEAR))
+	year = 2020
+	reportLines = getLinesFromInput()
+	numbersInReport = map(lambda x:int(x), reportLines)
+	numbersWhoAddUp  = findNumbersThatAddUpTo(numbersInReport, year)
+	print numbersWhoAddUp[0] * numbersWhoAddUp[1]
 
 solution()
